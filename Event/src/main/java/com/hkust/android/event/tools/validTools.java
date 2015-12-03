2e3a4c46654fcb111603bf5b9ef0de7b1e30dddc
sam <wangzhenyu1260@126.com>
2015-12-03 15:19:37 +0800

    login part and validation

---
+++ Event/src/main/java/com/hkust/android/event/tools/validTools.java
@@ -0,0 +1,25 @@
+package com.hkust.android.event.tools;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by sam on 15/12/1.
+ */
+public class validTools {
+    /*
+    * validate email tool*/
+    public boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+    public static boolean isValidateName(String name) {
+        String regx = "^[\\p{L} .'-]+$";
+        Pattern pattern = Pattern.compile(regx,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.find();
+    }
+}
