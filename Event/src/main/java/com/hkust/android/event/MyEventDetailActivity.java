307ab3ab817b6eabe363bd4943cede0a30e8621c
sam <wangzhenyu1260@126.com>
2015-12-04 18:57:31 +0800

    探索活动和我的活动

--- Event/src/main/java/com/hkust/android/event/MyEventDetailActivity.java
+++ Event/src/main/java/com/hkust/android/event/MyEventDetailActivity.java
@@ -1,6 +1,8 @@
 package com.hkust.android.event;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,8 +10,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.hkust.android.event.model.Constants;
+import com.hkust.android.event.model.Event;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MyEventDetailActivity extends AppCompatActivity implements View.OnClickListener{
+
+    String eventId;
+    private SharedPreferences sp;
+    private TextView event_title;
+    private TextView event_holder;
+    private TextView event_date;
+    private TextView event_time;
+    private TextView event_location;
+    private TextView event_desc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +56,66 @@ public class MyEventDetailActivity extends AppCompatActivity implements View.OnC
 
         Button closeDiscussionBtn = (Button)findViewById(R.id.close_discussion_btn);
         closeDiscussionBtn.setOnClickListener(this);
+
+        event_title = (TextView) findViewById(R.id.event_title_textView);
+        event_holder = (TextView) findViewById(R.id.event_holder_textView);
+        event_date = (TextView) findViewById(R.id.date_textView);
+        event_time = (TextView) findViewById(R.id.time_textView);
+        event_location = (TextView) findViewById(R.id.location_textView);
+        event_desc = (TextView) findViewById(R.id.event_discription_textView);
+
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if (extras == null) {
+                eventId = null;
+            } else {
+                eventId = extras.getString("eventId");
+            }
+        } else {
+            eventId = (String) savedInstanceState.getSerializable("eventId");
+        }
+
+
+        sp = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String token = sp.getString("token", "");
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("token", token);
+        params.put("id", eventId);
+        client.post(Constants.SERVER_URL + Constants.EVENT_DETAIL, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String response = new String(responseBody);
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String message = jsonObject.getString("message");
+                    if (message.equalsIgnoreCase("succeed")) {
+                        String eventString = jsonObject.getString("act");
+                        Gson gson = new Gson();
+                        Event event = gson.fromJson(eventString, Event.class);
+
+                        event_title.setText(event.getTitle());
+                        event_holder.setText(event.getHost().getName());
+                        event_time.setText(event.getTime());
+                        event_location.setText(event.getLocation().get(0));
+                        event_desc.setText(event.getDescription());
+                        event_date.setText(event.getStartAt()+" "+event.getEndAt());
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(getApplicationContext(), "Connection Failed", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
